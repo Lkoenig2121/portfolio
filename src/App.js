7ac8ef4ea3d4f9bcf7969bce5943fb9561aff898
Lukas Koenig <Lkoenig2121@gmail.com>
2023-05-30 14:19:53 -0400

    git init

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import Particles from "./components/Particles";
+import About from "./components/About";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Skills from "./components/Skills";
+import Work from "./components/Work";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+         
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Work />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
