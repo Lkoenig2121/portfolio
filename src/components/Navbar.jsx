cbb4a4edee4798f110dd31363daab67e44fa3f71
Lukas Koenig <Lkoenig2121@gmail.com>
2023-05-30 14:20:17 -0400

    git init

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,10 +9,6 @@ import Logo from '../assets/logo.png'
 const Navbar = () => {
     const [nav, setNav] = useState(false)
     const handleClick = () => setNav(!nav)
-
-    let Resume = function(){
-      window.open("./assets/Lukas_Koenig_Resume.png")
-    }
          
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300 z-10">
